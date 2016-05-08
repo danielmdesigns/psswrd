ed7fdaa37b21cfaa6a54faa0e28e3eb3e82a04d4
danielmdesigns <danielwmaldonado@gmail.com>
2016-05-07 22:42:29 -0500

    custom js
    custom js

---
+++ js/custom.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+
+"use strict";
+
+//if handheld, remove copy&paste function
+if(/Android|webOS|iPhone|iPad|iPod|BlackBerry/i.test(navigator.userAgent)){
+  $(".copy").hide();
+  $(".refresh").css({marginLeft:"0"});
+}
+
+//fittext.js
+$(".display").fitText(0.5, {
+  minFontSize: "40px",
+  maxFontSize: "140px"
+});
+
+});
