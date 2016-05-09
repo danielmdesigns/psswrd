9d7dc4fda5a4017a4679758e8124498cc89db159
Daniel Maldonado <daniel@danielmdesigns.com>
2016-05-09 08:21:56 -0500

    jQuery animation fix
    made sure all dom elements were being accessed by their js hooks.
    
    implemented a jQuery solution to my spinning button animation. jQuery
    worked best for 2 reasons:
    _1. I could determine the beginning & end of my animation
    _2. Avoid jumpy-ness of said animation by simply adding & removing a
    class at the start/end of the animation duration

--- js/custom.js
+++ js/custom.js
@@ -1,11 +1,21 @@
 //if handheld, remove copy&paste function
 if(/Android|webOS|iPhone|iPad|iPod|BlackBerry/i.test(navigator.userAgent)){
-  $(".copy").hide();
-  $(".refresh").css({marginLeft:"0"});
+  $("#js-copy").hide();
+  $("#js-refresh").css({marginLeft:"0"});
 }
 
 //fittext.js
-$(".display").fitText(0.5, {
+$("#js-display").fitText(0.5, {
   minFontSize: "40px",
   maxFontSize: "140px"
+});
+
+
+$("#js-refresh").click(function(){ 
+  "use strict";
+  $("#js-refresh").toggleClass("btn-spin");
+  $("#js-refresh").on('webkitAnimationEnd oanimationend msAnimationEnd animationend',   
+  function(){
+	  $("#js-refresh").removeClass("btn-spin");
+  });
 });
